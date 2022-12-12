174af4d2314e8205d4b3214a999964d6d922519d
suhrobfrontend <sukhrobfrontend@gmail.com>
2022-12-12 16:35:57 +0500

    update

--- js/main.js
+++ js/main.js
@@ -13,25 +13,39 @@ var road;
 
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  road = +elInput.value;
+  road = elInput.value;
+
+  function time(el, speed) {
+    var allHour = road / speed;
+    var floatMinute = floatTime(allHour);
+    var hour = allHour - floatTime(allHour);
+    var minute = (floatMinute * 60).toFixed(0);
+
+    if (hour !== 0) {
+      el.textContent = `${hour} soat-u, ${minute} daqiqa`;
+    } else {
+      el.textContent = `${minute} daqiqa`;
+    }
+  }
+
+  function floatTime(a) {
+    return a - Math.trunc(a);
+  }
+
+  if (isNaN(road)) {
+    alert("Raqam kiriting:");
+    elInput.value = "";
+    return;
+  }
+
+  if (road < 0) {
+    alert("Musbat son kiriting!");
+    elInput.value = "";
+    return;
+  }
+  if (road < 0) return;
   time(elTextFoot, foot);
   time(elTextBike, bike);
   time(elTextCar, car);
   time(elTextPlane, plane);
 });
-function time(el, speed) {
-  var allHour = road / speed;
-  var floatMinute = floatTime(allHour);
-  var hour = allHour - floatTime(allHour);
-  var minute = (floatMinute * 60).toFixed(0);
-
-  if (hour !== 0) {
-    el.textContent = `${hour} soat-u, ${minute} daqiqa`;
-  } else {
-    el.textContent = `${minute} daqiqa`;
-  }
-}
-
-function floatTime(a) {
-  return a - Math.trunc(a);
-}
