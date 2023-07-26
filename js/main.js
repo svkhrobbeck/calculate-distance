1a99ea51b5a585204a374925c3b0d068eb9e60a6
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-27 01:02:57 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-elForm = document.querySelector("[data-form]");
-elInput = document.querySelector("[data-distance]");
-elTextFoot = document.querySelector("[data-foot]");
-elTextBike = document.querySelector("[data-bike]");
-elTextCar = document.querySelector("[data-car]");
-elTextPlane = document.querySelector("[data-plane]");
+const elForm = document.querySelector("[data-form]"),
+  elInput = document.querySelector("[data-distance]"),
+  elTextFoot = document.querySelector("[data-foot]"),
+  elTextBike = document.querySelector("[data-bike]"),
+  elTextCar = document.querySelector("[data-car]"),
+  elTextPlane = document.querySelector("[data-plane]");
 
-var foot = 3.6;
-var bike = 20.1;
-var car = 70;
-var plane = 800;
-var road;
+let foot = 3.6;
+let bike = 20.1;
+let car = 70;
+let plane = 800;
+let road;
 
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   road = elInput.value;
 
   function time(el, speed) {
-    var allHour = road / speed;
-    var floatMinute = floatTime(allHour);
-    var hour = allHour - floatTime(allHour);
-    var minute = (floatMinute * 60).toFixed(0);
+    let allHour = road / speed;
+    let floatMinute = floatTime(allHour);
+    let hour = allHour - floatTime(allHour);
+    let minute = (floatMinute * 60).toFixed(0);
 
     if (hour !== 0) {
       el.textContent = `${hour} soat-u, ${minute} daqiqa`;
